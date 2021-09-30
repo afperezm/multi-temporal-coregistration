f55c9f65dee5030d4e0c79b1a9a2291229f41698
Felipe Perez <perezmaf@myumanitoba.ca>
2021-09-30 02:45:19 -0500

    Organize code as python program with prog args

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
+import argparse
 import os
 import torch
 import torch.utils.data as data
+from torch.utils.data import DataLoader
 
 from data import ImageFolder
 from framework import MyFrame
@@ -9,66 +11,79 @@ from networks.dinknet import DinkNet34
 from time import time
 
 SHAPE = (1024, 1024)
-ROOT = 'dataset/train/'
-image_list = list(filter(lambda x: x.find('sat') != -1, os.listdir(ROOT)))
-train_list = list(map(lambda x: x[:-8], image_list))
-NAME = 'log01_dink34'
-BATCH_SIZE_PER_CARD = 4
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
-solver = MyFrame(DinkNet34, DiceBCELoss, device, 2e-4)
-if torch.cuda.device_count() > 0:
-    batch_size = torch.cuda.device_count() * BATCH_SIZE_PER_CARD
-else:
-    batch_size = BATCH_SIZE_PER_CARD
+def main():
+    image_list = list(filter(lambda x: x.find('sat') != -1, os.listdir(PARAMS.data_dir)))
+    train_list = list(map(lambda x: x[:-8], image_list))
 
-dataset = ImageFolder(train_list, ROOT)
-data_loader = torch.utils.data.DataLoader(
-    dataset,
-    batch_size=batch_size,
-    shuffle=True,
-    num_workers=4)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-my_log = open('logs/' + NAME + '.log', 'w')
-tic = time()
-no_optimization = 0
-total_epoch = 300
-train_epoch_best_loss = 100.
-for epoch in range(1, total_epoch + 1):
-    data_loader_iter = iter(data_loader)
-    train_epoch_loss = 0
-    for img, mask in data_loader_iter:
-        solver.set_input(img, mask)
-        train_loss = solver.optimize()
-        train_epoch_loss += train_loss
-    train_epoch_loss /= len(data_loader_iter)
-    print('********', file=my_log)
-    print('epoch:', epoch, '    time:', int(time() - tic), file=my_log)
-    print('train_loss:', train_epoch_loss, file=my_log)
-    print('SHAPE:', SHAPE, file=my_log)
-    print('********')
-    print('epoch:', epoch, '    time:', int(time() - tic))
-    print('train_loss:', train_epoch_loss)
-    print('SHAPE:', SHAPE)
+    solver = MyFrame(DinkNet34, DiceBCELoss, device, 2e-4)
 
-    if train_epoch_loss >= train_epoch_best_loss:
-        no_optimization += 1
+    if torch.cuda.device_count() > 0:
+        batch_size = torch.cuda.device_count() * PARAMS.batch_size
     else:
-        no_optimization = 0
-        train_epoch_best_loss = train_epoch_loss
-        solver.save('weights/' + NAME + '.th')
-    if no_optimization > 6:
-        print('early stop at %d epoch' % epoch, file=my_log)
-        print('early stop at %d epoch' % epoch)
-        break
-    if no_optimization > 3:
-        if solver.old_lr < 5e-7:
-            break
-        solver.load('weights/' + NAME + '.th')
-        solver.update_lr(5.0, factor=True, my_log=my_log)
-    my_log.flush()
+        batch_size = PARAMS.batch_size
 
-print('Finish!', file=my_log)
-print('Finish!')
-my_log.close()
+    train_dataset = ImageFolder(train_list, PARAMS.data_dir)
+    train_dataloader = DataLoader(train_dataset,
+                                  batch_size=batch_size,
+                                  shuffle=True,
+                                  num_workers=4)
+
+    my_log = open('logs/' + PARAMS.name + '.log', 'w')
+    tic = time()
+    no_optimization = 0
+    total_epoch = 300
+    train_epoch_best_loss = 100.
+    for epoch in range(1, total_epoch + 1):
+        data_loader_iter = iter(train_dataloader)
+        train_epoch_loss = 0
+        for img, mask in data_loader_iter:
+            solver.set_input(img, mask)
+            train_loss = solver.optimize()
+            train_epoch_loss += train_loss
+        train_epoch_loss /= len(data_loader_iter)
+        print('********', file=my_log)
+        print('epoch:', epoch, '    time:', int(time() - tic), file=my_log)
+        print('train_loss:', train_epoch_loss, file=my_log)
+        print('SHAPE:', SHAPE, file=my_log)
+        print('********')
+        print('epoch:', epoch, '    time:', int(time() - tic))
+        print('train_loss:', train_epoch_loss)
+        print('SHAPE:', SHAPE)
+
+        if train_epoch_loss >= train_epoch_best_loss:
+            no_optimization += 1
+        else:
+            no_optimization = 0
+            train_epoch_best_loss = train_epoch_loss
+            solver.save('weights/' + PARAMS.name + '.th')
+        if no_optimization > 6:
+            print('early stop at %d epoch' % epoch, file=my_log)
+            print('early stop at %d epoch' % epoch)
+            break
+        if no_optimization > 3:
+            if solver.old_lr < 5e-7:
+                break
+            solver.load('weights/' + PARAMS.name + '.th')
+            solver.update_lr(5.0, factor=True, my_log=my_log)
+        my_log.flush()
+
+    print('Finish!', file=my_log)
+    print('Finish!')
+    my_log.close()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", help="Dataset directory", required=True)
+    parser.add_argument("--batch_size", help="Batch size", type=int, required=True)
+    parser.add_argument("--name", help="Model name", default="log01_dink34")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    PARAMS = parse_args()
+    main()
