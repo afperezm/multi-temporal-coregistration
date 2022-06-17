48e287cdb4d30bc0db0b9eb21aa4c1695c680a08
Felipe Perez <perezmaf@myumanitoba.ca>
2022-06-17 02:24:21 -0500

    Torchify topoloss implementation

--- topoloss.py
+++ topoloss.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# =============================================================================
-# Created By  : Xiaoling Hu
-# Created Date: Tue June 22 9:00:00 PDT 2021
-# =============================================================================
-
-import gudhi as gd
-import math
-import numpy as np
 import torch
+import torch_topological.nn as tt
 
 
 def compute_dgm_force(lh_dgm, gt_dgm, pers_thresh=0.03, pers_thresh_perfect=0.99, do_return_perfect=False):
@@ -32,41 +23,40 @@ def compute_dgm_force(lh_dgm, gt_dgm, pers_thresh=0.03, pers_thresh_perfect=0.99
         process
 
     """
-    lh_pers = abs(lh_dgm[:, 1] - lh_dgm[:, 0])
+    lh_pers = torch.abs(lh_dgm[:, 1] - lh_dgm[:, 0])
     if gt_dgm.shape[0] == 0:
         gt_pers = None
         gt_n_holes = 0
     else:
         gt_pers = gt_dgm[:, 1] - gt_dgm[:, 0]
-        gt_n_holes = gt_pers.size  # number of holes in gt
+        gt_n_holes = gt_pers.size(dim=0)  # number of holes in gt
 
     if gt_pers is None or gt_n_holes == 0:
-        idx_holes_to_fix = list()
-        idx_holes_to_remove = list(set(range(lh_pers.size)))
-        idx_holes_perfect = list()
+        idx_holes_to_fix = torch.tensor([])
+        idx_holes_to_remove = torch.range(0, lh_pers.size(dim=0) - 1, dtype=torch.int)
+        idx_holes_perfect = torch.tensor([])
     else:
         # check to ensure that all gt dots have persistence 1
-        tmp = gt_pers > pers_thresh_perfect
+        # tmp = torch.gt(gt_pers, pers_thresh_perfect)
 
         # get "perfect holes" - holes which do not need to be fixed, i.e., find top
         # lh_n_holes_perfect indices
         # check to ensure that at least one dot has persistence 1; it is the hole
         # formed by the padded boundary
         # if no hole is ~1 (ie >.999) then just take all holes with max values
-        tmp = lh_pers > pers_thresh_perfect  # old: assert tmp.sum() >= 1
-        lh_pers_sorted_indices = np.argsort(lh_pers)[::-1]
-        if np.sum(tmp) >= 1:
-            lh_n_holes_perfect = tmp.sum()
-            idx_holes_perfect = lh_pers_sorted_indices[:lh_n_holes_perfect]
+        tmp = torch.gt(lh_pers, pers_thresh_perfect)  # old: assert tmp.sum() >= 1
+        lh_pers_sorted_indices = torch.flip(torch.argsort(lh_pers), dims=[0])
+        if torch.sum(tmp) >= 1:
+            idx_holes_perfect = lh_pers_sorted_indices[:torch.sum(tmp)]
         else:
-            idx_holes_perfect = list()
+            idx_holes_perfect = torch.tensor([])
 
         # find top gt_n_holes indices
         idx_holes_to_fix_or_perfect = lh_pers_sorted_indices[:gt_n_holes]
 
         # the difference is holes to be fixed to perfect
-        idx_holes_to_fix = list(
-            set(idx_holes_to_fix_or_perfect) - set(idx_holes_perfect))
+        idx_holes_to_fix = idx_holes_to_fix_or_perfect[
+            torch.ne(idx_holes_to_fix_or_perfect.view(1, -1), idx_holes_perfect.view(-1, 1)).all(dim=0)]
 
         # remaining holes are all to be removed
         idx_holes_to_remove = lh_pers_sorted_indices[gt_n_holes:]
@@ -74,19 +64,19 @@ def compute_dgm_force(lh_dgm, gt_dgm, pers_thresh=0.03, pers_thresh_perfect=0.99
     # only select the ones whose persistence is large enough
     # set a threshold to remove meaningless persistence dots
     pers_thd = pers_thresh
-    idx_valid = np.where(lh_pers > pers_thd)[0]
-    idx_holes_to_remove = list(
-        set(idx_holes_to_remove).intersection(set(idx_valid)))
+    idx_valid = torch.where(torch.gt(lh_pers, pers_thd))[0]
+    idx_holes_to_remove = idx_holes_to_remove[
+        torch.eq(idx_holes_to_remove.view(1, -1), idx_valid.view(-1, 1)).any(dim=0)]
 
-    force_list = np.zeros(lh_dgm.shape)
+    force_list = torch.zeros_like(lh_dgm)
 
     # push each hole-to-fix to (0,1)
     force_list[idx_holes_to_fix, 0] = 0 - lh_dgm[idx_holes_to_fix, 0]
     force_list[idx_holes_to_fix, 1] = 1 - lh_dgm[idx_holes_to_fix, 1]
 
     # push each hole-to-remove to (0,1)
-    force_list[idx_holes_to_remove, 0] = lh_pers[idx_holes_to_remove] / math.sqrt(2.0)
-    force_list[idx_holes_to_remove, 1] = -lh_pers[idx_holes_to_remove] / math.sqrt(2.0)
+    force_list[idx_holes_to_remove, 0] = lh_pers[idx_holes_to_remove] / torch.sqrt(torch.tensor(2.0))
+    force_list[idx_holes_to_remove, 1] = -lh_pers[idx_holes_to_remove] / torch.sqrt(torch.tensor(2.0))
 
     if do_return_perfect:
         return force_list, idx_holes_to_fix, idx_holes_to_remove, idx_holes_perfect
@@ -109,29 +99,26 @@ def get_critical_points(likelihood):
 
     """
     lh = 1 - likelihood
-    lh_vector = np.asarray(lh).flatten()
 
-    lh_cubic = gd.CubicalComplex(
-        dimensions=[lh.shape[0], lh.shape[1]],
-        top_dimensional_cells=lh_vector
-    )
+    # Compute persistence diagram
+    cc_mod = tt.CubicalComplex()
+    cc = cc_mod(lh)
 
-    diag_lh = lh_cubic.persistence(homology_coeff_field=2, min_persistence=0)
-    pairs_lh = lh_cubic.cofaces_of_persistence_pairs()
+    # Return empty tensors and false to skip if there are no pairings for dimension 0
+    if len(cc[0].pairing) == 0:
+        return torch.tensor([[]]), torch.tensor([[]]), torch.tensor([[]]), False
 
-    # If the paris is 0, return False to skip
-    if len(pairs_lh[0]) == 0 or len(pairs_lh[0][0]) == 0:
-        return 0, 0, 0, False
-
-    # return persistence diagram, birth/death critical points
-    pd_lh = np.array([[lh_vector[pairs_lh[0][0][i][0]], lh_vector[pairs_lh[0][0][i][1]]] for i in range(len(pairs_lh[0][0]))])
-    bcp_lh = np.array([[pairs_lh[0][0][i][0] // lh.shape[1], pairs_lh[0][0][i][0] % lh.shape[1]] for i in range(len(pairs_lh[0][0]))])
-    dcp_lh = np.array([[pairs_lh[0][0][i][1] // lh.shape[1], pairs_lh[0][0][i][1] % lh.shape[1]] for i in range(len(pairs_lh[0][0]))])
+    # Compute persistence diagram
+    pd_lh = lh.view(-1)[torch.mm(cc[0].pairing.view(-1, 2), torch.tensor([[lh.shape[0]], [1]])).view(-1, 2)]
+    # Compute birth critical points
+    bcp_lh = cc[0].pairing.view(-1, 2)[[i for i in range(0, cc[0].pairing.view(-1, 2).size(dim=0), 2)]]
+    # Compute death critical points
+    dcp_lh = cc[0].pairing.view(-1, 2)[[i for i in range(1, cc[0].pairing.view(-1, 2).size(dim=0), 2)]]
 
     return pd_lh, bcp_lh, dcp_lh, True
 
 
-def get_topo_loss(likelihood_tensor, gt_tensor, topo_size=100):
+def get_topo_loss(likelihood, gt, topo_size=100):
     """
     Calculate the topology loss of the predicted image and ground truth image
     Warning: To make sure the topology loss is able to back-propagation, likelihood
@@ -139,23 +126,17 @@ def get_topo_loss(likelihood_tensor, gt_tensor, topo_size=100):
     likelihood tensor to GPUs device.
 
     Args:
-        likelihood_tensor:   The likelihood pytorch tensor.
-        gt_tensor        :   The groundtruth of pytorch tensor.
-        topo_size        :   The size of the patch is used. Default: 100
+        likelihood:   The likelihood pytorch tensor.
+        gt        :  The groundtruth of pytorch tensor.
+        topo_size :  The size of the patch is used. Default: 100
 
     Returns:
-        loss_topo        :   The topology loss value (tensor)
+        loss_topo :   The topology loss value (tensor)
 
     """
 
-    likelihood = likelihood_tensor.clone()
-    gt = gt_tensor.clone()
-
-    likelihood = torch.squeeze(likelihood).cpu().detach().numpy()
-    gt = torch.squeeze(gt).cpu().detach().numpy()
-
-    topo_cp_weight_map = np.zeros(likelihood.shape)
-    topo_cp_ref_map = np.zeros(likelihood.shape)
+    topo_cp_weight_map = torch.zeros_like(likelihood, dtype=torch.float)
+    topo_cp_ref_map = torch.zeros_like(likelihood, dtype=torch.float)
 
     for y in range(0, likelihood.shape[0], topo_size):
         for x in range(0, likelihood.shape[1], topo_size):
@@ -163,9 +144,9 @@ def get_topo_loss(likelihood_tensor, gt_tensor, topo_size=100):
             lh_patch = likelihood[y:min(y + topo_size, likelihood.shape[0]), x:min(x + topo_size, likelihood.shape[1])]
             gt_patch = gt[y:min(y + topo_size, gt.shape[0]), x:min(x + topo_size, gt.shape[1])]
 
-            if np.min(lh_patch) == 1 or np.max(lh_patch) == 0:
+            if torch.min(lh_patch) == 1 or torch.max(lh_patch) == 0:
                 continue
-            if np.min(gt_patch) == 1 or np.max(gt_patch) == 0:
+            if torch.min(gt_patch) == 1 or torch.max(gt_patch) == 0:
                 continue
 
             # Get the critical points of predictions and ground truth
@@ -180,7 +161,7 @@ def get_topo_loss(likelihood_tensor, gt_tensor, topo_size=100):
 
             force_list, idx_holes_to_fix, idx_holes_to_remove = compute_dgm_force(pd_lh, pd_gt, pers_thresh=0.03)
 
-            if len(idx_holes_to_fix) > 0 or len(idx_holes_to_remove) > 0:
+            if idx_holes_to_fix.size(dim=0) > 0:
                 for hole_index in idx_holes_to_fix:
                     if int(bcp_lh[hole_index][0]) >= 0 and int(bcp_lh[hole_index][0]) < likelihood.shape[0] and int(bcp_lh[hole_index][1]) >= 0 and int(bcp_lh[hole_index][1]) < likelihood.shape[1]:
                         topo_cp_weight_map[y + int(bcp_lh[hole_index][0]), x + int(bcp_lh[hole_index][1])] = 1  # push birth to 0 i.e. min birth prob or likelihood
@@ -188,6 +169,8 @@ def get_topo_loss(likelihood_tensor, gt_tensor, topo_size=100):
                     if int(dcp_lh[hole_index][0]) >= 0 and int(dcp_lh[hole_index][0]) < likelihood.shape[0] and int(dcp_lh[hole_index][1]) >= 0 and int(dcp_lh[hole_index][1]) < likelihood.shape[1]:
                         topo_cp_weight_map[y + int(dcp_lh[hole_index][0]), x + int(dcp_lh[hole_index][1])] = 1  # push death to 1 i.e. max death prob or likelihood
                         topo_cp_ref_map[y + int(dcp_lh[hole_index][0]), x + int(dcp_lh[hole_index][1])] = 1
+
+            if idx_holes_to_remove.size(dim=0) > 0:
                 for hole_index in idx_holes_to_remove:
                     if int(bcp_lh[hole_index][0]) >= 0 and int(bcp_lh[hole_index][0]) < likelihood.shape[0] and int(bcp_lh[hole_index][1]) >= 0 and int(bcp_lh[hole_index][1]) < likelihood.shape[1]:
                         topo_cp_weight_map[y + int(bcp_lh[hole_index][0]), x + int(bcp_lh[hole_index][1])] = 1  # push birth to death  # push to diagonal
@@ -202,10 +185,7 @@ def get_topo_loss(likelihood_tensor, gt_tensor, topo_size=100):
                         else:
                             topo_cp_ref_map[y + int(dcp_lh[hole_index][0]), x + int(dcp_lh[hole_index][1])] = 0
 
-    topo_cp_weight_map = torch.tensor(topo_cp_weight_map, dtype=torch.float).to(likelihood_tensor.device)
-    topo_cp_ref_map = torch.tensor(topo_cp_ref_map, dtype=torch.float).to(likelihood_tensor.device)
-
     # Measuring the MSE loss between predicted critical points and reference critical points
-    loss_topo = (((likelihood_tensor * topo_cp_weight_map) - topo_cp_ref_map) ** 2).sum()
+    loss_topo = (((likelihood * topo_cp_weight_map) - topo_cp_ref_map) ** 2).sum()
 
     return loss_topo
