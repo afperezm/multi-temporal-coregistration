c3462851ef73d2d10ca23d1e52552fada51446fe
Felipe Perez <perezmaf@myumanitoba.ca>
2022-12-03 08:09:18 -0800

    Add scripts to train folds

---
+++ scripts/run.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0000/train/"
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0000
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0000
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0000
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0000
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0000
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0001/train/"
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0001
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0001
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0001
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0001
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0001
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0002/train/"
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0002
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0002
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0002
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0002
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0002
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0003/train/"
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0003
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0003
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0003
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0003
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0003
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0004/train/"
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0004
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0004
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0004
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0004
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0004
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0005/train/"
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0005
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0005
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0005
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0005
+
+python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0005
