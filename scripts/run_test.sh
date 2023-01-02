210afe5f5afb8af4a58bed87fae0ea17fea635e1
Felipe Perez <perezmaf@myumanitoba.ca>
2023-01-02 13:15:14 -0500

    Add script to test models

---
+++ scripts/run_test.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo -e "\n##############\n### FOLD 0 ###\n##############\n"
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0000/test/"
+
+for ckpt_dir in weights/dlinknet34-imagenet-gillam-all-season-fold-0000-*; do
+  python -u test.py \
+         --data_dir "${data_dir}" \
+         --output_dir ./submits/ \
+         --checkpoints_dir "${ckpt_dir}" \
+         --model "model"
+done
+
+echo -e "\n##############\n### FOLD 1 ###\n##############\n"
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0001/test/"
+
+for ckpt_dir in weights/dlinknet34-imagenet-gillam-all-season-fold-0001-*; do
+  python -u test.py \
+         --data_dir "${data_dir}" \
+         --output_dir ./submits/ \
+         --checkpoints_dir "${ckpt_dir}" \
+         --model "model"
+done
+
+echo -e "\n##############\n### FOLD 2 ###\n##############\n"
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0002/test/"
+
+for ckpt_dir in weights/dlinknet34-imagenet-gillam-all-season-fold-0002-*; do
+  python -u test.py \
+         --data_dir "${data_dir}" \
+         --output_dir ./submits/ \
+         --checkpoints_dir "${ckpt_dir}" \
+         --model "model"
+done
+
+echo -e "\n##############\n### FOLD 3 ###\n##############\n"
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0003/test/"
+
+for ckpt_dir in weights/dlinknet34-imagenet-gillam-all-season-fold-0003-*; do
+  python -u test.py \
+         --data_dir "${data_dir}" \
+         --output_dir ./submits/ \
+         --checkpoints_dir "${ckpt_dir}" \
+         --model "model"
+done
+
+echo -e "\n##############\n### FOLD 4 ###\n##############\n"
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0004/test/"
+
+for ckpt_dir in weights/dlinknet34-imagenet-gillam-all-season-fold-0004-*; do
+  python -u test.py \
+         --data_dir "${data_dir}" \
+         --output_dir ./submits/ \
+         --checkpoints_dir "${ckpt_dir}" \
+         --model "model"
+done
+
+echo -e "\n##############\n### FOLD 5 ###\n##############\n"
+
+data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0005/test/"
+
+for ckpt_dir in weights/dlinknet34-imagenet-gillam-all-season-fold-0005-*; do
+  python -u test.py \
+         --data_dir "${data_dir}" \
+         --output_dir ./submits/ \
+         --checkpoints_dir "${ckpt_dir}" \
+         --model "model"
+done
--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "\n##############\n### FOLD 0 ###\n##############\n"
+
 data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0000/train/"
 
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0000
@@ -11,6 +13,8 @@ python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-ima
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0000
 
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0000
+
+echo -e "\n##############\n### FOLD 1 ###\n##############\n"
 
 data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0001/train/"
 
@@ -24,6 +28,8 @@ python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-ima
 
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0001
 
+echo -e "\n##############\n### FOLD 2 ###\n##############\n"
+
 data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0002/train/"
 
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0002
@@ -35,6 +41,8 @@ python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-ima
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0002
 
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0002
+
+echo -e "\n##############\n### FOLD 3 ###\n##############\n"
 
 data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0003/train/"
 
@@ -48,6 +56,8 @@ python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-ima
 
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0003
 
+echo -e "\n##############\n### FOLD 4 ###\n##############\n"
+
 data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0004/train/"
 
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0004
@@ -59,6 +69,8 @@ python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-ima
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0004
 
 python -u train.py --data_dir "${data_dir}" --batch_size 8 --name dlinknet34-imagenet-gillam-all-season-fold-0004
+
+echo -e "\n##############\n### FOLD 5 ###\n##############\n"
 
 data_dir="/Users/perezmaf/data/northern-cities/gillam_mb_canada/fold-0005/train/"
 
