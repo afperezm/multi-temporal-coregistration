e505f739e53c70cb8685aa1ba83d6a58f300d488
Felipe Perez <perezmaf@myumanitoba.ca>
2023-01-26 06:43:42 -0500

    Fix baseline dir naming

--- scripts/run_folds_register_baseline.sh
+++ scripts/run_folds_register_baseline.sh
@@ -4,28 +4,28 @@ data_dir="${HOME}/data/northern-cities/gillam_mb_canada/"
 
 python -u utils/baseline_coregister.py \
        --data_dir "${data_dir}/folds/S2_10m/" \
-       --output_dir "${data_dir}/folds/S2_10m_shifted/" \
+       --output_dir "${data_dir}/folds/S2_10m_deshifted/" \
        --match_band 3
 
 for fold in 0 1 2 3 4 5; do
 
   echo -e "\n##############\n### FOLD ${fold} ###\n##############\n"
 
-  ln -s "${data_dir}/folds/fold-000${fold}/train_wkt.csv" "${data_dir}/folds/fold-000${fold}/train_shifted_wkt.csv"
-  ln -s "${data_dir}/folds/fold-000${fold}/test_wkt.csv" "${data_dir}/folds/fold-000${fold}/test_shifted_wkt.csv"
+  ln -s "${data_dir}/folds/fold-000${fold}/train_wkt.csv" "${data_dir}/folds/fold-000${fold}/train_deshifted_wkt.csv"
+  ln -s "${data_dir}/folds/fold-000${fold}/test_wkt.csv" "${data_dir}/folds/fold-000${fold}/test_deshifted_wkt.csv"
 
-  for phase in train_shifted test_shifted; do
+  for phase in train_deshifted test_deshifted; do
 
     python -u utils/dataset_images.py \
            --data_dir "${data_dir}/folds/fold-000${fold}/" \
-           --images_dir "${data_dir}/folds/S2_10m_shifted/" \
+           --images_dir "${data_dir}/folds/S2_10m_deshifted/" \
            --phase "${phase}" \
            --threshold 0.001 \
            --num_workers 8
 
   done
 
-  rm "${data_dir}/folds/fold-000${fold}/train_shifted_wkt.csv"
-  rm "${data_dir}/folds/fold-000${fold}/test_shifted_wkt.csv"
+  rm "${data_dir}/folds/fold-000${fold}/train_deshifted_wkt.csv"
+  rm "${data_dir}/folds/fold-000${fold}/test_deshifted_wkt.csv"
 
 done
