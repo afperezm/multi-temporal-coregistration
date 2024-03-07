0df00fefbf1b2c77a90fd08642882f2ad4d55694
Felipe Perez <perezmaf@myumanitoba.ca>
2024-03-07 18:40:34 -0500

    Parse submit directory with split subdirectory

--- utils/dataset_quality.py
+++ utils/dataset_quality.py
@@ -31,37 +31,35 @@ def main():
     submits_dir = PARAMS.submits_dir
     band = PARAMS.band
 
-    data_dirs = sorted(glob.glob(os.path.join(submits_dir, 'dlinknet34-imagenet-gillam-all-season-fold-000[1-5]-[0-9]*-[0-9]*')))
-
     folds = []
     ssim_scores = []
 
-    for data_dir in data_dirs:
+    submits_root_dir = os.path.dirname(os.path.abspath(submits_dir))
 
-        fold = int(os.path.basename(os.path.split(data_dir)[-1]).split('-')[6])
+    fold = int(os.path.basename(submits_root_dir).split('-')[-3])
 
-        print(f'fold={fold}')
+    print(f'fold={fold}')
 
-        scores = []
+    scores = []
 
-        for eopatch_idx in get_patch_indices(data_dir):
+    for eopatch_idx in get_patch_indices(submits_dir):
 
-            print(f'eopatch-{eopatch_idx:04d}')
+        print(f'eopatch-{eopatch_idx:04d}')
 
-            image_paths = sorted(glob.glob(os.path.join(data_dir, f'*_eopatch-{eopatch_idx:04d}-{eopatch_idx:04d}_*.jpg')))
+        image_paths = sorted(glob.glob(os.path.join(submits_dir, f'*_eopatch-{eopatch_idx:04d}-{eopatch_idx:04d}_*.jpg')))
 
-            for tgt_path, ref_path in zip(image_paths[-1:] + image_paths[:-1], image_paths):
-                ref = cv2.imread(ref_path)
-                tgt = cv2.imread(tgt_path)
-                score = ssim(normalize(ref[:, :, band]), normalize(tgt[:, :, band]), data_range=1)
-                scores.append(score)
+        for tgt_path, ref_path in zip(image_paths[-1:] + image_paths[:-1], image_paths):
+            ref = cv2.imread(ref_path)
+            tgt = cv2.imread(tgt_path)
+            score = ssim(normalize(ref[:, :, band]), normalize(tgt[:, :, band]), data_range=1)
+            scores.append(score)
 
-        folds.append(fold)
-        ssim_scores.append(np.mean(scores))
+    folds.append(fold)
+    ssim_scores.append(np.mean(scores))
 
-    if not os.path.exists(os.path.join(submits_dir, 'ssim_scores.pkl')):
+    if not os.path.exists(os.path.join(submits_root_dir, 'ssim_scores.pkl')):
         df = pd.DataFrame(list(zip(folds, ssim_scores)), columns=['Fold', 'SSIM'])
-        df.to_pickle(os.path.join(submits_dir, 'ssim_scores.pkl'))
+        df.to_pickle(os.path.join(submits_root_dir, 'ssim_scores.pkl'))
 
 
 def parse_args():
